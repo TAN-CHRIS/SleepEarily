85e905d85437705fd8f1b230f4dba24dfdbf0265
qiaqi <2903561172@qq.com>
2018-11-16 20:12:14 +0800

    update

--- app/src/main/java/com/example/chris/sleepearily/MainNaviActivity.java
+++ app/src/main/java/com/example/chris/sleepearily/MainNaviActivity.java
@@ -11,6 +11,7 @@ public class MainNaviActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -19,6 +20,7 @@ public class MainNaviActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    setContentView(R.layout.fragment_blank);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -39,6 +41,7 @@ public class MainNaviActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }
